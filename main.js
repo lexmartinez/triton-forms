5e034d2fa7ca1bbbfafb6e358bafed862f2c653e
Lex Martinez <lex0316@gmail.com>
2016-09-17 09:26:25 -0500

    initial setup

---
+++ main.js
@@ -0,0 +1,35 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+let mainWindow
+
+function createWindow () {
+
+  mainWindow = new BrowserWindow({width: 1024, height: 768,
+        resizable: true, moveable:true, title: "triton-forms"})
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools()
+
+  mainWindow.maximize()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
